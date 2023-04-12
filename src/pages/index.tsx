46c99626dd8a36beb9f485930bb95cc95d237f07
dinhngocnam <nam.dn@qasoftsolution.com>
2023-04-12 17:29:47 +0700

    delete table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,14 +23,6 @@ export default function App() {
   const [authToken, setAuthToken] = useState("");
   const [users, setUsers] = useState<Array<UserType>>([]);
   const router = useRouter();
-  const columns = [
-    { name: "ID", uid: "id" },
-    { name: "USERNAME", uid: "username" },
-    { name: "EMAIL", uid: "email" },
-    { name: "CREATED TIME", uid: "created_at" },
-    { name: "UPDATED TIME", uid: "updated_at" },
-    { name: "ACTIONS", uid: "actions" },
-  ];
 
   useEffect(() => {
     const setToken = async () => {
@@ -60,50 +52,6 @@ export default function App() {
     getContacts();
   }, [currentUser, authToken, setUsers]);
 
-  const handleDelete = async (user_id: number) => {
-    const { data } = await deleteUser(authToken, user_id);
-    if (data === "Deleted Successfully!") {
-      router.reload();
-    }
-  };
-
-  const renderCell = (user: UserType, columnKey: React.Key) => {
-    const cellValue = user[columnKey];
-    switch (columnKey) {
-      case "actions":
-        return (
-          <Row justify="center" align="center">
-            <Col css={{ d: "flex" }}>
-              <Tooltip content="Details">
-                <IconButton onClick={() => console.log("View user", user?.id)}>
-                  <EyeIcon size={20} fill="#979797" />
-                </IconButton>
-              </Tooltip>
-            </Col>
-            <Col css={{ d: "flex" }}>
-              <Tooltip content="Edit user">
-                <IconButton onClick={() => console.log("Edit user", user?.id)}>
-                  <EditIcon size={20} fill="#979797" />
-                </IconButton>
-              </Tooltip>
-            </Col>
-            <Col css={{ d: "flex" }}>
-              <Tooltip
-                content="Delete user"
-                color="error"
-                onClick={() => handleDelete(user?.id)}
-              >
-                <IconButton>
-                  <DeleteIcon size={20} fill="#FF0080" />
-                </IconButton>
-              </Tooltip>
-            </Col>
-          </Row>
-        );
-      default:
-        return cellValue;
-    }
-  };
   return (
     <>
       <Head>
@@ -116,38 +64,6 @@ export default function App() {
         <div>
           <h1>Chào mừng {currentUser?.username} đến với trang chủ</h1>
         </div>
-        <div>
-          <h2>Danh sách Users</h2>
-        </div>
-        <Table
-          aria-label="Example table with custom cells"
-          css={{
-            height: "auto",
-            minWidth: "100%",
-          }}
-          selectionMode="none"
-        >
-          <Table.Header columns={columns}>
-            {(column) => (
-              <Table.Column
-                key={column.uid}
-                hideHeader={column.uid === "actions"}
-                align={column.uid === "actions" ? "center" : "start"}
-              >
-                {column.name}
-              </Table.Column>
-            )}
-          </Table.Header>
-          <Table.Body items={users}>
-            {(item: UserType) => (
-              <Table.Row>
-                {(columnKey) => (
-                  <Table.Cell>{renderCell(item, columnKey)}</Table.Cell>
-                )}
-              </Table.Row>
-            )}
-          </Table.Body>
-        </Table>
       </main>
     </>
   );
